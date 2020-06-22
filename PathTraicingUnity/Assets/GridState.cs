00588da779325c73a74876b47bc2fa8723340b5e
Bastien PERROTEAU <bastienperoteau@gmail.com>
2020-06-22 12:07:32 +0200

    State Ready

--- PathTraicingUnity/Assets/GridState.cs
+++ PathTraicingUnity/Assets/GridState.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GridState : MonoBehaviour
 {
     [SerializeField]
-    private int Size = 24;
+    private int Size = 34;
     [SerializeField]
     private Transform Field = null;
     [SerializeField]
@@ -40,7 +40,7 @@ public class GridState : MonoBehaviour
             for (int cpty = 0; cpty < Size; cpty++)
             {
                 if (cptx == 0 || cpty == 0 || cptx == Size - 1 || cpty == Size - 1) State[cptx, cpty] = 0;
-                if (cptx == 1 || cpty == 1 || cptx == Size - 2 || cpty == Size - 2) State[cptx, cpty] = 1;
+                else if (cptx == 1 || cpty == 1 || cptx == Size - 2 || cpty == Size - 2) State[cptx, cpty] = 1;
                 else State[cptx, cpty] = Random.Range(1, 6);
             }
         }
@@ -53,17 +53,17 @@ public class GridState : MonoBehaviour
     {
         DeleteField();
         // 0 = Océan
-        // 1 = Plaine
-        // 2 = Foret
-        // 3 = Plateau
-        // 4 = Montagne
-        // 5 = Lac
-        Material Forest = Resources.Load("/Hexagon/Forest", typeof(Material)) as Material;
-        Material Plain = Resources.Load("/Hexagon/Plain", typeof(Material)) as Material;
-        Material Mountain = Resources.Load("/Hexagon/Mountain", typeof(Material)) as Material;
-        Material Plateau = Resources.Load("/Hexagon/Plateau", typeof(Material)) as Material;
-        Material Ocean = Resources.Load("/Hexagon/Ocean", typeof(Material)) as Material;
-        Material Lac = Resources.Load("/Hexagon/Lac", typeof(Material)) as Material;
+        // 1 = Lac
+        // 2 = Plaine
+        // 3 = Foret
+        // 4 = Plateau
+        // 5 = Montagne
+        Material Forest = Resources.Load("Forest", typeof(Material)) as Material;
+        Material Plain = Resources.Load("Plain", typeof(Material)) as Material;
+        Material Mountain = Resources.Load("Mountain", typeof(Material)) as Material;
+        Material Plateau = Resources.Load("Plateau", typeof(Material)) as Material;
+        Material Ocean = Resources.Load("Ocean", typeof(Material)) as Material;
+        Material Lac = Resources.Load("Lac", typeof(Material)) as Material;
         for (int cptx = 0; cptx < Size; cptx++)
         {
             for (int cpty = 0; cpty < Size; cpty++)
@@ -73,7 +73,7 @@ public class GridState : MonoBehaviour
                 float PosY;
                 if (cptx % 2 == 0)
                 {
-                    PosX = ((cptx%Size) * 7.7f);
+                    PosX = ((cptx % Size) * 7.7f);
                     PosY = (cpty * 8.8f);
                 }
                 else
@@ -82,31 +82,31 @@ public class GridState : MonoBehaviour
                     PosY = (cpty * 8.8f) + 4.4f;
                 }
                 Go.transform.position = new Vector3(PosX, PosY, 0);
-                /*
+                Go.transform.SetParent(Field);
+                if (cptx == 0 || cpty == 0 || cptx == Size - 1 || cpty == Size - 1) Debug.Log("State[" + cptx + ", " + cpty + "] : " + State[cptx, cpty]);
                 switch (State[cptx, cpty])
                 {
                     case 0:
                         Go.GetComponent<Renderer>().material = Ocean;
                         break;
                     case 1:
-                        Go.GetComponent<Renderer>().material = Plain;
+                        Go.GetComponent<MeshRenderer>().material = Lac;
                         break;
                     case 2:
-                        Go.GetComponent<Renderer>().material = Forest;
+                        Go.GetComponent<MeshRenderer>().material = Plain;
                         break;
                     case 3:
-                        Go.GetComponent<Renderer>().material = Plateau;
-                        Go.transform.localScale = new Vector3(Go.transform.localScale.x, Go.transform.localScale.y, Go.transform.localScale.z * 1.3f);
+                        Go.GetComponent<MeshRenderer>().material = Forest;
                         break;
                     case 4:
-                        Go.GetComponent<Renderer>().material = Mountain;
-                        Go.transform.localScale = new Vector3(Go.transform.localScale.x, Go.transform.localScale.y, Go.transform.localScale.z * 1.6f);
+                        Go.GetComponent<MeshRenderer>().material = Plateau;
+                        Go.transform.localScale = new Vector3(Go.transform.localScale.x, Go.transform.localScale.y, Go.transform.localScale.z * 1.3f);
                         break;
                     case 5:
-                        Go.GetComponent<Renderer>().material = Lac;
+                        Go.GetComponent<MeshRenderer>().material = Mountain;
+                        Go.transform.localScale = new Vector3(Go.transform.localScale.x, Go.transform.localScale.y, Go.transform.localScale.z * 1.6f);
                         break;
                 }
-                */
                 Go.SetActive(true);
             }
         }
@@ -117,6 +117,6 @@ public class GridState : MonoBehaviour
     }
     void InitCam()
     {
-        GameObject.Find("MainCamera").transform.position = new Vector3(Size/2 * 7.7f, Size / 2 * 8.8f, -(Size) * 10);
+        GameObject.Find("MainCamera").transform.position = new Vector3(Size / 2 * 7.7f, Size / 2 * 8.8f, -(Size) * 7.7f);
     }
 }
